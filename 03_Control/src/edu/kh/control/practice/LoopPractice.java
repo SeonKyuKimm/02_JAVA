9a7559cb511cbd3abe242ecb2b767b8106438a4b
SeonKyuKimm <ksg112809@gmail.com>
2024-02-06 01:46:43 +0900

    240206night

---
+++ 03_Control/src/edu/kh/control/practice/LoopPractice.java
@@ -0,0 +1,58 @@
+package edu.kh.control.practice;
+
+import java.util.Scanner;
+
+public class LoopPractice {
+
+	public void practice1(){
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print(" 1 이상의 숫자를 입력하세요 : ");
+		int input = sc.nextInt();
+		
+		if ( input >= 1){
+			
+			for(int i = 1; i <= input; i++) {
+				System.out.print( i + " ");
+			}
+		} else {
+			System.out.println("1 이상의 숫자를 입력하세요.");
+		}
+	}
+	
+	public void practice2() {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print(" 1 이상의 숫자를 입력하세요 : ");
+		int input2 = sc.nextInt();
+		
+		if (input2 >= 1) {
+			for(int i = input2; i >= 1; i--) {
+				System.out.print(i + " ");
+			}
+		}else {
+			System.out.println(" 1 이상의 숫자를 입력하세요.");
+		}
+	}
+	
+	public void practice3() {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("정수를 하나 입력하세요 : ");
+		int input3 = sc.nextInt();
+		
+		int sum = 0;
+		
+		for(int i = 1; i <= input3-1 ; i++) {
+			
+			sum += i;
+			System.out.print( i + " + " );
+		}
+		sum += input3;
+		System.out.print(input3 + " = " + sum);
+	}
+}
+
