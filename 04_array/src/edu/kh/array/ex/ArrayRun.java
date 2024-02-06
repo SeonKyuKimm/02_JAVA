223b2f834b54b1366235a3fb42c4a6e72c4c80bb
SeonKyuKimm <ksg112809@gmail.com>
2024-02-06 17:44:36 +0900

    240206class

---
+++ 04_array/src/edu/kh/array/ex/ArrayRun.java
@@ -0,0 +1,25 @@
+package edu.kh.array.ex;
+
+public class ArrayRun {
+
+	public static void main(String[] args) { // 실행용클래스
+		
+		ArrayExample arEx = new ArrayExample();
+	
+		//arEx.ex1();
+		//arEx.ex2();
+		//arEx.ex3();
+		//arEx.ex4();
+		//arEx.ex5();
+		//arEx.ex6();
+		//arEx.ex7();
+		//arEx.ex8();
+		//arEx.ex9();
+		
+		ArrayExample2 arEx2 = new ArrayExample2();
+
+		//arEx2.shallowCopy();
+		//arEx2.deepCopy();
+		//arEx2.createLottoNumber();
+	}
+}
